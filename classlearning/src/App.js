cb50676ec949f1e17e3212a60288af3cd87c371f
karthikyelisetti <62472452+karthikyelisetti@users.noreply.github.com>
2023-05-24 07:59:40 +0530

    WIP: Learning Props

--- classlearning/src/App.js
+++ classlearning/src/App.js
@@ -2,12 +2,24 @@
 import "./App.css";
 import Hello from "./components/Hello";
 import Header from "./components/Header";
+import Contactus from "./components/Contactus";
 
 function App() {
+  const contactdetails = {
+    name: "Karthik",
+    phone: "9999999999",
+    email: "karthikyeilsetti.dev@gmail.com",
+  };
+  const salutation = ["namaste", "hello", "hola", "bonjour"];
   return (
     <div className="App">
       <Header></Header>
-      <Hello></Hello>
+      <Contactus
+        name={contactdetails.name}
+        phone={contactdetails.phone}
+        email={contactdetails.email}
+        salutation={salutation}
+      ></Contactus>
     </div>
   );
 }
