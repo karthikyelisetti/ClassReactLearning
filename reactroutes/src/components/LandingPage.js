a7955abc5340bf1f922614028e221e78f8558d06
karthikyelisetti <62472452+karthikyelisetti@users.noreply.github.com>
2023-06-03 18:54:54 +0530

    useeffect API calling

--- reactroutes/src/components/LandingPage.js
+++ reactroutes/src/components/LandingPage.js
@@ -1,17 +1,32 @@
 import React ,{useEffect, useState} from 'react'
+import axios from 'axios';
+// axios: library
+//fetch: ajax
 
 // use effect
 // hook responsible for side effects like api calling
 export default function LandingPage() {
     const [counter, setCounter] = useState(0);
     console.log("I am rendering in landing page component");
-    let a = 10;
+    // let a = 10;
 
     useEffect(() => {
         console.log("I am getting printed from useeffect body..");
 
-        let bdy = document.querySelector(".header")
-    }, [counter])
+        const apicalling = async() => {
+            try {
+                let response = await axios.get("https://dummyjson.com/products");
+                console.log(response.data);
+            }catch(error) {
+                console.log(error);
+            }
+        }
+
+        apicalling();
+
+        // let body = document.querySelector(".header");
+        // console.log(body);
+    }, [])
     //[]: dependency array
     // case1: dependency array is empty: useeffect runs only single time when the page renders
     // case2: dependency array has some state or props: whwnever the state or props changes, your useeffect will trigger that many times
