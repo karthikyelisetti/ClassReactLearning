c931620acdfa5a185051326ae2c4e8dd12f87327
karthikyelisetti <62472452+karthikyelisetti@users.noreply.github.com>
2023-06-03 18:30:27 +0530

    WIP: useeffect dependency array

--- reactroutes/src/components/LandingPage.js
+++ reactroutes/src/components/LandingPage.js
@@ -1,8 +1,9 @@
-import React ,{useEffect} from 'react'
+import React ,{useEffect, useState} from 'react'
 
 // use effect
 // hook responsible for side effects like api calling
 export default function LandingPage() {
+    const [counter, setCounter] = useState(0);
     console.log("I am rendering in landing page component");
     let a = 10;
 
@@ -12,10 +13,15 @@ export default function LandingPage() {
         let bdy = document.querySelector(".header")
     }, [])
     //[]: dependency array
-
-    console.log("loads second");
+    // case1: dependency array is empty: useeffect runs only single time when the page renders
+    // case2: dependency array has some state or props: whwnever the state or props changes, your useeffect will trigger that many times
 
     return (
-        <div className='header'>LandingPage :{a}</div>
+        <div className='header'>
+            {/* In line calling of stateFunction using arrow function */}
+            <button onClick={() => setCounter(counter+1)}>+</button>
+            {counter}
+            {/* LandingPage :{a} */}
+        </div>
     )
 }
